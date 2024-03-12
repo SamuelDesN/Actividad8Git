41dd8518c88728f40300231e7f5e7b44a237e47c
SamuelDesN <vazquezsamuel877@gmail.com>
2024-03-12 03:24:11 +0100

    Creacion de los metodos que necesitaremos para realizar el ejercicio

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -26,5 +26,35 @@ public class Biblioteca {
         this.libros = libros;
     }
 
+    public void agregarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
 
+    public void agregarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    public void mostrarLibrosDisponibles() {
+        System.out.println("Libros disponibles:");
+        for (Libro libro : libros) {
+            System.out.println(libro.getTitulo() + " - " + libro.getAutor() + " - " + libro.getIsbn());
+        }
+    }
+
+    public void mostrarLibrosPendientes(Usuario usuario) {
+        System.out.println("Libros pendientes de devolucion para " + usuario.getNombre() + ":");
+        for (Libro libro : usuario.getLibrosPendientes()) {
+            System.out.println(libro.getTitulo() + " - " + libro.getAutor() + " - " + libro.getIsbn());
+        }
+    }
+
+    public void pedirLibro(Usuario usuario, Libro libro) {
+        usuario.agregarLibroPendiente(libro);
+        System.out.println(usuario.getNombre() + " ha pedido el libro " + libro.getTitulo());
+    }
+
+    public void devolverLibro(Usuario usuario, Libro libro) {
+        usuario.devolverLibro(libro);
+        System.out.println(usuario.getNombre() + " ha devuelto el libro " + libro.getTitulo());
+    }
 }
